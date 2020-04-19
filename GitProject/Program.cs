3dbcd519e26cc5741c99e158f854399acd68c2fd
Marcelo Felipe <mfecosta11@hotmail.com>
2020-04-19 18:50:41 -0300

    Criei um Boa noite

--- GitProject/Program.cs
+++ GitProject/Program.cs
@@ -7,6 +7,7 @@ namespace GitProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Boa noite");
         }
     }
 }
